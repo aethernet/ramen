5ad666c56bba95b8214f47ece4718548d1382c55
Edwin Joassart <edwin@hlo.io>
2020-05-26 15:59:32 +0200

    panzoom on touch interface (mobile)

--- src/components/EditorViewport/hooks/useZoomingAndPanning.ts
+++ src/components/EditorViewport/hooks/useZoomingAndPanning.ts
@@ -8,6 +8,9 @@ import {
   stopPanning,
 } from "../../../redux/viewport/viewport.actions"
 import { IStoreState } from "../../../redux/types";
+import AlloyFinger from "alloyfinger"
+
+let temp0 = {}
 
 function useZoomingAndPanning(canZoom: boolean, canPan: boolean, viewport: any): null {
   const dispatch = useDispatch();
@@ -19,6 +22,8 @@ function useZoomingAndPanning(canZoom: boolean, canPan: boolean, viewport: any):
 
   React.useEffect(() => {
     if (!viewport) return;
+
+    const  af = new AlloyFinger(viewport, {});
 
     // zoom on alt + scroll
     function onWheelScroll(e: WheelEvent) {
@@ -32,26 +37,39 @@ function useZoomingAndPanning(canZoom: boolean, canPan: boolean, viewport: any):
       }
     }
 
-    //FIXME: this doesnt work
-    function onPointerMove(e: MouseEvent) {
-      console.log(e, isPanning)
-      if (!isPanning && !e.altKey && !e.buttons) return
-      if (!isPanning) dispatch(startPanning(e.pageX, e.pageY))
-      if (isPanning && (!e.altKey || !e.buttons)) dispatch(stopPanning())
-      if (isPanning) dispatch(setViewportPos(e.pageX, e.pageY))
-      
-      e.preventDefault()
-      e.stopPropagation()
+    function onPinch(e: TouchEvent) {
+      const zoom = 1-e.zoom
+      if(zoom < -0.5 || zoom > 0.5) //prevent accidental zoom when drag
+        dispatch(setViewportZoom(zoom*3, e.pageX, e.pageY))
     }
+
+    function ontwoFingerPressMove(e: TouchEvent) {
+      dispatch(setViewportPosDelta(-e.deltaX, -e.deltaY))
+    }
+
+    // //FIXME: this doesnt work
+    // function onPointerMove(e: MouseEvent) {
+    //   if (!isPanning && !e.buttons) return
+    //   if (!isPanning) dispatch(startPanning(e.pageX, e.pageY))
+    //   if (isPanning && (!e.altKey || !e.buttons)) dispatch(stopPanning())
+    //   if (isPanning) dispatch(setViewportPos(e.pageX, e.pageY))
+      
+    //   e.preventDefault()
+    //   e.stopPropagation()
+    // }
+
 
     viewport.addEventListener("wheel", onWheelScroll);
     if(canPan){
-      viewport.addEventListener("pointermove", onPointerMove);
+      af.on("twoFingerPressMove", ontwoFingerPressMove);
+    }
+    if(canZoom){
+      af.on("pinch", onPinch);
     }
 
     return () => {
       viewport.removeEventListener("wheel", onWheelScroll);
-      viewport.removeEventListener("pointermove", onPointerMove);
+      af.destroy();
     };
 
   }, [canZoom, viewport]);
